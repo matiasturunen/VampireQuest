149cd3b1cda96170b389a5c513353c89dc28b3b3
Matias Turunen <mt95@windowslive.com>
2016-08-28 23:11:30 +0300

    Play audio in some places

--- Assets/Scripts/Player.js
+++ Assets/Scripts/Player.js
@@ -6,21 +6,29 @@ public var ammo : int = 10;
 public var healthDrainSpeed: float = 1;  // remove amount of HP from player every N seconds
 public var healthDrainAmount: float = 1; // amount of HP to remove over time
 public var bloodDrinkingSpeed: float = 5;
+public var reloadTime: float = 0.2;
 
 public var projectile : GameObject;
 public var deathParticles : ParticleSystem;
 public var loot : GameObject[];
+
+public var reloadSound: AudioClip;
+private var audioSource: AudioSource;
 
 private var hud : HUD;
 private var animator : Animator;
 private var rigidBody : Rigidbody2D;
 private var nextHealthDrainTime: float = healthDrainSpeed;
 private var nextBloodDrinkTime: float = 1;
+private var nextBoltFireTime: float = 0;
 
 
 function Start() {
   rigidBody = GetComponent(Rigidbody2D);
   animator = GetComponent(Animator);
+
+  audioSource = GetComponent(AudioSource);
+  audioSource.playOnAwake = false;
 
   hud = GameObject.Find("HUD").GetComponent(HUD);
 
@@ -100,17 +108,23 @@ function AddAmmo(amount : int) {
 }
 
 function FireWeapon() {
-  if (ammo > 0) {
-    ammo -= 1;
-    var spawnPos : Vector3 = Vector3(
-      transform.position.x + transform.up.x * 1.1,
-      transform.position.y + transform.up.y * 1.1,
-      0
-    );
-    Instantiate(projectile, spawnPos, transform.rotation);
-  }
+  if (Time.time > nextBoltFireTime) {
+    nextBoltFireTime = Time.time + reloadTime;
+    if (ammo > 0) {
+      ammo -= 1;
+      var spawnPos : Vector3 = Vector3(
+        transform.position.x + transform.up.x * 1.1,
+        transform.position.y + transform.up.y * 1.1,
+        0
+      );
+      Instantiate(projectile, spawnPos, transform.rotation);
+      audioSource.clip = reloadSound;
+      audioSource.volume = 0.4;
+      audioSource.Play();
+    }
 
-  Debug.Log("Fire, actor (" + gameObject.ToString() + "), ammo remains (" + ammo.ToString() + ")");
+    Debug.Log("Fire, actor (" + gameObject.ToString() + "), ammo remains (" + ammo.ToString() + ")");
+  }
 }
 
 function OnTriggerStay2D(coll: Collider2D) {
